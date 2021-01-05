7ec163985b70894473f5ad0ca42ca357fd17c2ac
Adame <Adame.haouari@gmail.com>
2021-01-05 10:32:05 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+// let a = 1; 
+// let b = 2; 
+// let c = 3; 
+
+// console.log(a, b, c);
+
+
+
+// let temp = a
+// a = b
+// b = c
+// c = temp
+// console.log(a, b, c);
+
+
+
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+
+// let tempp = a
+// let temppp = b
+// a = d
+// b = c
+// c = temppp
+// d = tempp
+
+// console.log(a, b, c, d);
+
+
+
+// let tab = [3 , 2, 1]
+// tab.unshift(4)
+// console.log(tab);
+// tab.sort()
+// console.log(tab)
+// Exo 4
+
+let a = 1 
+let b = 2 
+let c = 3 
+let d = 4 
+let e = 5
+let f = 6 
+
+let temp = a
+let tempp = b
+let temppp = c
+
+a = d
+b = e
+c = temp
+d = tempp
+e = f
+f = temppp
+console.log(a ,b , c , d ,e ,f);
+
+
+
